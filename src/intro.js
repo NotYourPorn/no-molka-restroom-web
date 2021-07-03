49c5b4e4ae1c7dad3e36f0ae0056d70338a59bde
Saebyeol Ju <86865245+NotYourPorn@users.noreply.github.com>
2021-07-03 22:24:52 +0900

    initial setting

---
+++ src/intro.js
@@ -0,0 +1,21 @@
+/** @format */
+
+import React from "react";
+
+const intro = () => {
+  return (
+    <div className="container">
+      <div className="slogan">
+        <h1>My Life is NOT Your Porn</h1>
+      </div>
+      <div>
+        <h2>몰래 카메라는 처벌 받아야하는 불법 촬영 '범죄'입니다.</h2>
+      </div>
+      <div className="btn">
+        <button>STOP</button>
+      </div>
+    </div>
+  );
+};
+
+export default intro;
