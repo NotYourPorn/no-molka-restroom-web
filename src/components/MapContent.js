abf889d818adda1730b4d41f8a8a2d58d23be2b4
Saebyeol Ju <86865245+NotYourPorn@users.noreply.github.com>
2021-10-06 14:24:29 +0900

    [Map] 검색 시 지도 좌표 이동 성공

--- src/components/MapContent.js
+++ src/components/MapContent.js
@@ -25,9 +25,12 @@ const MapContent = () => {
     const container = document.getElementById("map");
     const options = {
       center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
-      level: 4,
+      level: 6,
     };
     const map = new kakao.maps.Map(container, options);
+
+    var bounds = new kakao.maps.LatLngBounds();
+    var Pos = [];
 
     function displayMarker(city, placePos, place) {
       let marker = new kakao.maps.Marker({
@@ -35,6 +38,12 @@ const MapContent = () => {
         position: new kakao.maps.LatLng(placePos[0], placePos[1]),
         title: place.name,
       });
+    }
+
+    function setBounds() {
+      // LatLngBounds 객체에 추가된 좌표들을 기준으로 지도의 범위를 재설정합니다
+      // 이때 지도의 중심좌표와 레벨이 변경될 수 있습니다
+      map.setBounds(bounds);
     }
 
     if (loading) {
@@ -45,9 +54,16 @@ const MapContent = () => {
 
       data.cities.map((city) => {
         return city.places.map((place) => {
-          return <>{displayMarker(city, trimPos(place.pos[0]), place)}</>;
+          return (
+            <>
+              {(Pos = trimPos(place.pos[0]))}
+              {displayMarker(city, Pos, place)}
+              {bounds.extend(new kakao.maps.LatLng(Pos[0], Pos[1]))}
+            </>
+          );
         });
       });
+      setBounds();
     }
   });
 
