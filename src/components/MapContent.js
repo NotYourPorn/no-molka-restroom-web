3c8ad91a85953e015c3ba7a3bcc9dd16cabe860e
Saebyeol Ju <86865245+NotYourPorn@users.noreply.github.com>
2021-10-09 15:56:56 +0900

    [Map] Marker 이미지 수정, infowindow 수정

--- src/components/MapContent.js
+++ src/components/MapContent.js
@@ -5,27 +5,11 @@ import React, { useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
 import { GET_MAP_INFO, FETCH_CITIES_QUERY } from "../server/Data/RentQueries";
 import * as GoIcons from "react-icons/go";
-import keyIcon from "../img/key.png";
+import keyIcon from "../img/rent_marker_1.png";
 
 const trimPos = (pos) => {
   const tempPos = pos.replaceAll("'", "");
   return pos.slice(1, tempPos.length - 1).split(", ");
-};
-
-const infoWindow = (map, place, city, placePos) => {
-  var iwContent = `<div style="padding:5px;" className = "map-info-box">${place.name}<div>주소 : ${place.address}</div></div>`, // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-    iwPosition = new kakao.maps.LatLng(placePos[0], placePos[1]), //인포윈도우 표시 위치입니다
-    iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
-
-  // 인포윈도우를 생성하고 지도에 표시합니다
-  var infowindow = new kakao.maps.InfoWindow({
-    map: map, // 인포윈도우가 표시될 지도
-    position: iwPosition,
-    content: iwContent,
-    removable: iwRemoveable,
-  });
-
-  infowindow.close();
 };
 
 const MapContent = () => {
@@ -51,7 +35,7 @@ const MapContent = () => {
     var Pos = [];
 
     var imageSrc = keyIcon, // 마커이미지의 주소입니다
-      imageSize = new kakao.maps.Size(24, 24), // 마커이미지의 크기입니다
+      imageSize = new kakao.maps.Size(29, 33), // 마커이미지의 크기입니다
       imageOption = { offset: new kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
     var markerImage = new kakao.maps.MarkerImage(
@@ -59,12 +43,33 @@ const MapContent = () => {
       imageSize,
       imageOption
     );
+
     function displayMarker(city, placePos, place) {
       let marker = new kakao.maps.Marker({
         map: map,
         position: new kakao.maps.LatLng(placePos[0], placePos[1]),
         title: place.name,
         image: markerImage,
+        clickable: true,
+      });
+
+      var iwContent = `<div style="padding:5px;" class = "map-window"><div class="title">${place.name}</div><div class = "desc"><ul>${place.address}</ul><ul>${place.phone}</ul></div></div>`, // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+        iwPosition = new kakao.maps.LatLng(placePos[0], placePos[1]), //인포윈도우 표시 위치입니다
+        iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+
+      // 인포윈도우를 생성하고 지도에 표시합니다
+      var infowindow = new kakao.maps.InfoWindow({
+        map: map, // 인포윈도우가 표시될 지도
+        position: iwPosition,
+        content: iwContent,
+        removable: iwRemoveable,
+      });
+
+      infowindow.close();
+
+      kakao.maps.event.addListener(marker, "click", function () {
+        // 마커 위에 인포윈도우를 표시합니다
+        infowindow.open(map, marker);
       });
     }
 
@@ -78,20 +83,14 @@ const MapContent = () => {
       console.log("this is loading");
     }
     if (data) {
-      console.log("this is data");
-
-      data.cities.map((city) => {
-        return city.places.map((place) => {
-          return (
-            <>
-              {(Pos = trimPos(place.pos[0]))}
-              {displayMarker(city, Pos, place)}
-              {bounds.extend(new kakao.maps.LatLng(Pos[0], Pos[1]))}
-              {infoWindow(map, place, city, Pos)}
-            </>
-          );
+      data.cities.forEach((city) => {
+        city.places.forEach((place) => {
+          Pos = trimPos(place.pos[0]);
+          displayMarker(city, Pos, place);
+          bounds.extend(new kakao.maps.LatLng(Pos[0], Pos[1]));
         });
       });
+
       setBounds();
     }
   });
